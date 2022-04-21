908493215435824e0fa48739c6661ccb5755776a
devin <devinbalian@gmail.com>
2022-04-21 16:23:26 +0200

    finished check_orth

--- versuch1/check_orth.m
+++ versuch1/check_orth.m
@@ -7,9 +7,20 @@ bc=0;
 L=1;
 
 % Erstellen der CC matrix
+cc = createCC(n, ord, bc);
 
 % Gitterschrittweite bestimmen
+dx = L/n;
 
 % Lösen der Eigenwertgleichung mit solveCC
+[kx, modes] = solveCC(cc, dx);
 
 % Überprüfung der Orthogonalität der Eigenvektoren
+A = zeros(n,n);
+for i = 1:n
+    for j = 1:n
+        A(i,j) = modes(:,i)' * modes(:,j);
+    end
+end
+
+imagesc(A);
