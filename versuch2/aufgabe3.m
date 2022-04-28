ed3ad1b8933bcc91d73b409339fee1a18eaab6d1
ChristophReinwald <christoph_juergen.reinwald@stud.tu-darmstadt.de>
2022-04-28 20:56:38 +0200

    Plot Beispielgitter erstellt

--- versuch2/aufgabe3.m
+++ versuch2/aufgabe3.m
@@ -4,6 +4,14 @@
 
 %% Beispielgitter definieren (3D)
 
+xmesh = [1,2,3,6];
+ymesh = [1,2,4,5];
+zmesh = [1,2,3,4];
+
 % Erzeugen des Gitters
 
+[mesh] = cartMesh(xmesh, ymesh, zmesh);
+
 %% Darstellen des Gitters
+
+plotMesh(mesh);
