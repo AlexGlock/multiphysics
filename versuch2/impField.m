7428a56bc9012eb058dd31a212f6acf62817cb8e
Devin Balian <devinbalian@gmail.com>
2022-04-28 16:20:35 +0200

    impField and plotImpField

--- versuch2/impField.m
+++ versuch2/impField.m
@@ -22,26 +22,44 @@ nx = msh.nx; ny = msh.ny; nz = msh.nz;
 Mx = msh.Mx; My = msh.My; Mz = msh.Mz;
 xmesh = msh.xmesh; ymesh = msh.ymesh; zmesh = msh.zmesh;
 
+% Geisterkantenindizes
+edg = boundEdg(msh);
+
 % Bogenspannungsvektor initieren
 fieldBow = zeros(3*np,1);
 
-% Schleife über alle Punkte
+% Schleife ueber alle Punkte
 for i=1:nx
     for j=1:ny
         for k=1:nz
             % kanonischen Index n bestimmen
-            n = 
+            n = 1 + (i-1)*Mx + (j-1)*My + (k-1)*Mz;
+            if (any(edg==n))
+                continue
+            end
             
             % x-, y- und z-Koordinate des Gitterpunktes bestimmen
             x = xmesh(i);
             y = ymesh(j);
             z = zmesh(k);
+            % Kantenmittelpunkte bestimmen
+            xm = (x + xmesh(i+1))/2;
+            ym = (y + ymesh(j+1))/2;
+            zm = (z + zmesh(k+1))/2;
+            % Kantenlaengen bestimmen
+            xl = xmesh(i+1) - x;
+            yl = ymesh(i+1) - y;
+            zl = zmesh(i+1) - z;
 
-            % Bogenwert für x-Kante mit Index n
-            
-            % Bogenwert für y-Kante mit Index n
-            
-            % Bogenwert für z-Kante mit Index n
+            % Bogenwert fuer x-Kante mit Index n
+            fx = field(xm,y,z);
+            fieldBow(n) = fx(1) * xl;
+            % Bogenwert fuer y-Kante mit Index n
+            fy = field(x,ym,z);
+            fieldBow(n + np) = fy(2) * yl;
+            % Bogenwert fuer z-Kante mit Index n
+            fz = field(x,y,zm);
+            fieldBow(n + 2*np) = fz(3) * zl;
         end
     end
 end
--- versuch2/plotImpField.m
+++ versuch2/plotImpField.m
@@ -6,15 +6,20 @@
 
 %% Aufgabe 9
 % Beispielgitter erzeugen (3D)
+xmesh = [1, 4, 7];
+ymesh = [1, 3, 7, 8];
+zmesh = [1, 2, 5, 6, 8];
 
 %% msh struct berechnen
 msh = cartMesh(xmesh, ymesh, zmesh);
+xmin = xmesh(1);
+xmax = xmesh(end);
 
 %% anonymous function der beiden gegebenen Felder bestimmen
-% f1 = 
-% f2 = 
+f1 = @(x,y,z) [5/2 -1.3 2];
+f2 = @(x,y,z) [0 3*sin(pi*(x-xmin)/(xmax-xmin)) 0];
 
-%% Bogengröße der beiden Felder mithilfe von impField bestimmen
+%% Bogengroesse der beiden Felder mithilfe von impField bestimmen
 fbow1 = impField( msh, f1 );
 fbow2 = impField( msh, f2 );
 
