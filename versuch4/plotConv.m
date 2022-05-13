8aff640b977f5001185b17b904650dfd5ea32090
devin <devinbalian@gmail.com>
2022-05-13 16:47:09 +0200

    aufgabe 5

--- versuch4/plotConv.m
+++ versuch4/plotConv.m
@@ -10,14 +10,22 @@ zmesh = linspace(0,1,11);
 msh = cartMesh(xmesh, ymesh, zmesh);
 
 % Gewaehlten Kondensator definieren (Materialien, Potentiale)
-% ...
+boxesEps(1).box = [1, msh.nx, 1, msh.ny, 1, msh.nz];
+boxesEps(1).value = 1;
+
+boxesPot(1).box = [1, floor(msh.nx/2), ceil(msh.ny/2), msh.ny, 1, msh.nz];
+boxesPot(1).value = 1;
+boxesPot(2).box = [1, msh.nx, msh.ny, msh.ny, 1, msh.nz];
+boxesPot(2).value = 1;
+boxesPot(3).box = [1, msh.nx, 1, 1, 1, msh.nz];
+boxesPot(3).value = 0;
 
 % Berechnen der Permittivitaet mit boxMesher
-eps = boxMesher(   ,   ,   );
+eps = boxMesher(msh, boxesEps, 1);
 
 
 % Berechnen des Potentials mit boxMesher
-pot = boxMesher(   ,   ,   );
+pot = boxMesher(msh, boxesPot, NaN);
 
 
 % Erstellen des Ladungsvektors
